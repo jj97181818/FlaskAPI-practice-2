94dfcef114bf741ed1c53faca2dc582f368ae375
jj97181818 <jj97181818@gmail.com>
2019-05-14 23:50:54 +0800

    Do request parser on POST and PUT method

--- code/app.py
+++ code/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
 from security import authenticate, identity
 
@@ -17,12 +17,21 @@ class Item(Resource):
     def get(self, name):
         item = next(filter(lambda x: x['name'] == name, items), None)
         return {"item": item}, 200 if item else 404
-                
 
     def post(self, name):
+        parser = reqparse.RequestParser()
+        parser.add_argument(
+            'price',
+            type = float,
+            required = True,
+            help = "This field can not be blank."
+        )
+        data = parser.parse_args()
+
         if next(filter(lambda x: x['name'] == name, items), None):
             return {"message": "An item with name '{}' already exists.".format(name)}, 400 
-        data = request.get_json()
+        
+        
         item = {"name": name, "price": data['price']}
         items.append(item)
         return item, 201
@@ -33,13 +42,21 @@ class Item(Resource):
         return {'message': 'Item deleted '}
 
     def put(self, name):
-        data = request.get_json()
+        parser = reqparse.RequestParser()
+        parser.add_argument(
+            'price',
+            type = float,
+            required = True, # no request can come through without price
+            help = "This field can not be blank."
+        )
+        data = parser.parse_args()
+
         item = next(filter(lambda x: x['name'] == name, items), None)
         if item is None:
             item = {'name': name, 'price': data['price']}
             items.append(item)
         else:
-            item.update(data)
+            item.update(data) 
         return item
 
 class ItemList(Resource):
