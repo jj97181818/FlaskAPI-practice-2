d9c62578aa9abf219cb0bfb78d185ce61b5d5041
jj97181818 <jj97181818@gmail.com>
2019-05-24 17:04:29 +0800

    Implement Items using SQLAlchemy

--- resources/item.py
+++ resources/item.py
@@ -1,4 +1,3 @@
-import sqlite3
 from flask_restful import Resource, reqparse
 from flask_jwt import jwt_required
 from models.item import ItemModel
@@ -57,18 +56,5 @@ class Item(Resource):
 
 class ItemList(Resource):
     def get(self):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = "SELECT * FROM items"
-        result = cursor.execute(query)
-
-        items = []
-        for row in result:
-            items.append({'name': row[1], 'price': row[2]})
-
-        connection.close()
-        
-        return {"items": items}
-
-    
+        return {'items': [item.json() for item in ItemModel.query.all()]}
+        #return {'items': list(map(lambda x: x.json(), ItemModel.query.all()))}
